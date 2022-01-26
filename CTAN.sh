36cef7ff74bdb69c9e588ba149e47303f7d695a8
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2022-01-26 20:05:59 +0530

    Add files via upload

--- CTAN.sh
+++ CTAN.sh
@@ -1,4 +1,4 @@
-#last run: 
+#last run: Wednesday 
 #Program to download all CTAN Latex documentation and store it locally
 set -x
 cat <(echo "#last run:" $(date +'%c')) <(tail -n +2 "$0") > .temp
@@ -11,9 +11,8 @@ grep "<a href=\".*\.zip" |
 cut -b 85- | cut -d \. -f 1 |
 {
 	while read package; do
-		[ -d "Source/$package" ] || mkdir Source/$package
 		curl "https://ctan.math.washington.edu/tex-archive/macros/latex/contrib/$package.zip" > $package.zip
-		unzip $package.zip -d Source/$package
+		unzip $package.zip -d Source/
 		rm $package.zip
 		ln -s $(find Source/$package -name $package.pdf) Docs/$package.pdf
 	done
